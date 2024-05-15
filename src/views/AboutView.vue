d84b1f2f26d036bf79ed95c5a6db7b3d1d55c84a
Jolangalifk <jolangalifk@gmail.com>
2024-05-15 23:26:00 +0700

    add: [JK] responsive screen page about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,34 +1,34 @@
 <template>
-  <div class="flex flex-col lg:gap-32">
-    <div class="flex flex-col justify-between lg:m-[80px] lg:gap-[50px]">
-      <div class="flex flex-row justify-between lg:gap-[78px]">
-        <div class="flex flex-col lg:justify-between">
-          <h1 class="text-[100px] font-black text-[#0f1314] m-0">ZEZKUN</h1>
-          <p class="font-black text-2xl text-[#0f1314] m-0 lg:w-[50%]">THRIFT JACKET STORE</p>
+  <div class="flex flex-col sm:gap-16 lg:gap-32">
+    <div class="flex flex-col justify-between m-5 sm:m-10 lg:m-[80px] sm:gap-[50px]">
+      <div class="flex flex-col mb-10 sm:flex-row justify-between sm:gap-[39px] sm:m-0 lg:gap-[78px]">
+        <div class="flex flex-col sm:justify-between mb-4 sm:m-0">
+          <h1 class="text-6xl sm:text-7xl lg:text-[100px] font-black text-[#0f1314] m-0">ZEZKUN</h1>
+          <p class="font-black text-2xl text-[#0f1314] m-0 sm:w-[50%]">THRIFT JACKET STORE</p>
         </div>
-        <div class="lg:w-[784px] lg:h-[300px]">
+        <div class="sm:w-[784px] sm:h-[300px]">
           <img class="w-full h-full object-cover" src="@/assets/images/about-img-1.png" alt="About">
         </div>
       </div>
-      <div class="flex flex-row justify-between lg:gap-[78px]">
-        <div class="lg:w-[496px] lg:h-[150px]">
+      <div class="flex flex-row justify-between gap-10 sm:gap-[39px] lg:gap-[78px]">
+        <div class="sm:w-[784px] sm:h-[300px] lg:w-[496px] lg:h-[150px]">
           <img class="w-full h-full object-cover" src="@/assets/images/about-img-2.png" alt="Since">
         </div>
-        <div class="flex flex-row justify-between lg:items-end lg:gap-[30px]">
+        <div class="flex flex-col justify-center sm:flex-row sm:justify-between sm:items-end sm:gap-[30px]">
           <p class="font-black text-2xl text-[#0f1314] m-0">SINCE</p>
-          <h1 class="text-[100px] font-black text-[#0f1314] m-0">2024</h1>
+          <h1 class="text-6xl sm:text-7xl lg:text-[100px] font-black text-[#0f1314] m-0">2024</h1>
         </div>
       </div>
     </div>
-    <div class="flex flex-col text-[#0f1314] m-0 items-center lg:gap-20">
-      <h1 class="font-black lg:text-6xl">OUR HISTORY</h1>
-      <p class="text-2xl text-justify lg:w-[45%]">Starting out as a place for lovers of quality second-hand goods, zezkun was inspired by some of the iconic
+    <div class="flex flex-col text-[#0f1314] m-5 sm:m-10 items-center lg:mb-10 lg:m-0 lg:gap-20">
+      <h1 class="font-black mb-4 sm:mb-8 sm:text-4xl lg:text-6xl lg:mb-0">OUR HISTORY</h1>
+      <p class="text-sm sm:text-xl lg:text-2xl text-justify mb-4 sm:mb-8 lg:w-[45%] lg:mb-0">Starting out as a place for lovers of quality second-hand goods, zezkun was inspired by some of the iconic
         outdoor jacket brands in the mountaineering world. We believe that every climber deserves reliable and
         dependable gear, without having to sacrifice quality for price.</p>
       <div class="w-full">
         <img class="w-full h-full" src="@/assets/images/about-img-3.png" alt="History">
       </div>
-      <p class="text-2xl text-justify lg:w-[45%]">With a passionate and talented team, zezkun is committed to providing a fun and fulfilling shopping experience
+      <p class="text-sm sm:text-xl lg:text-2xl text-justify mt-4 sm:mt-8 lg:m-0 lg:w-[45%]">With a passionate and talented team, zezkun is committed to providing a fun and fulfilling shopping experience
         for climbers around the world. Although our focus is on second-hand outdoor jackets, our passion for exploring
         the outdoors and conquering mountain peaks is as strong as the air roaring on the mountaintops we love.</p>
     </div>
