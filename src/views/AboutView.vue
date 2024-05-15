7b11ea3f0f55b76368bab5f3a616b9a77fc63226
Jolangalifk <jolangalifk@gmail.com>
2024-05-15 21:23:46 +0700

    add: [JK] about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="flex flex-col lg:gap-32">
+    <div class="flex flex-col justify-between lg:m-[80px] lg:gap-[50px]">
+      <div class="flex flex-row justify-between lg:gap-[78px]">
+        <div class="flex flex-col lg:justify-between">
+          <h1 class="text-[100px] font-black text-[#0f1314] m-0">ZEZKUN</h1>
+          <p class="font-black text-2xl text-[#0f1314] m-0 lg:w-[50%]">THRIFT JACKET STORE</p>
+        </div>
+        <div class="lg:w-[784px] lg:h-[300px]">
+          <img class="w-full h-full object-cover" src="@/assets/images/about-img-1.png" alt="About">
+        </div>
+      </div>
+      <div class="flex flex-row justify-between lg:gap-[78px]">
+        <div class="lg:w-[496px] lg:h-[150px]">
+          <img class="w-full h-full object-cover" src="@/assets/images/about-img-2.png" alt="Since">
+        </div>
+        <div class="flex flex-row justify-between lg:items-end lg:gap-[30px]">
+          <p class="font-black text-2xl text-[#0f1314] m-0">SINCE</p>
+          <h1 class="text-[100px] font-black text-[#0f1314] m-0">2024</h1>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col text-[#0f1314] m-0 items-center lg:gap-20">
+      <h1 class="font-black lg:text-6xl">OUR HISTORY</h1>
+      <p class="text-2xl text-justify lg:w-[45%]">Starting out as a place for lovers of quality second-hand goods, zezkun was inspired by some of the iconic
+        outdoor jacket brands in the mountaineering world. We believe that every climber deserves reliable and
+        dependable gear, without having to sacrifice quality for price.</p>
+      <div class="w-full">
+        <img class="w-full h-full" src="@/assets/images/about-img-3.png" alt="History">
+      </div>
+      <p class="text-2xl text-justify lg:w-[45%]">With a passionate and talented team, zezkun is committed to providing a fun and fulfilling shopping experience
+        for climbers around the world. Although our focus is on second-hand outdoor jackets, our passion for exploring
+        the outdoors and conquering mountain peaks is as strong as the air roaring on the mountaintops we love.</p>
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
