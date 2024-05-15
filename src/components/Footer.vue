4e2881dc3d516bd795a93d200d0620731e2f624e
Jolangalifk <jolangalifk@gmail.com>
2024-05-15 13:52:53 +0700

    add: [JK] responsive screen footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="m-0 lg:p-20 bg-[#0f1314] flex flex-col lg:gap-5 w-full">
-        <div class="flex justify-between text-white lg:py-9 relative">
-            <div class="flex flex-col">
-                <h1 class="font-bold lg:text-xl m-0 lg:mb-4">ZEZKUN</h1>
-                <div class="flex flex-col gap-[10px]">
+    <div class="m-0 p-5 sm:p10 lg:p-20 bg-[#0f1314] flex flex-col lg:gap-5 w-full">
+        <div class="flex flex-col justify-between text-white mb-4 sm:mb-0 sm:py-4 lg:mb-0 lg:py-9 relative sm:flex-row">
+            <div class="flex flex-row items-center w-full gap-2 mb-4 sm:hidden">
+                <img class="w-[30%] lg:w-full" src="@/assets/images/zezkun-logo.png" alt="">
+            </div>
+            <div class="flex flex-col mb-4 lg:mb-0">
+                <h1 class="font-bold hidden lg:flex lg:text-xl m-0 lg:mb-4">ZEZKUN</h1>
+                <h1 class="font-bold text-xl m-0 mb-0 lg:hidden">Our Company</h1>
+                <div class="flex flex-col gap-1 lg:gap-[10px]">
                     <a>Discover</a>
                     <a>Our Story</a>
                     <a>Join Us</a>
                 </div>
             </div>
-            <div class="flex flex-col">
+            <div class="flex flex-col mb-4 lg:mb-0">
                 <h1 class="font-bold lg:text-xl m-0 lg:mb-4">INFORMATION</h1>
-                <div class="flex flex-col gap-[10px]">
+                <div class="flex flex-col gap-1 lg:gap-[10px]">
                     <a>My Account</a>
                     <a>Contact Us</a>
                     <a>FAQ</a>
@@ -19,39 +23,39 @@
                     <a>Shipping</a>
                 </div>
             </div>
-            <div class="flex flex-col">
+            <div class="flex flex-col mb-6 lg:mb-0">
                 <h1 class="font-bold lg:text-xl m-0 lg:mb-4">CATEGORIES</h1>
-                <div class="flex flex-col gap-[10px]">
+                <div class="flex flex-col gap-1 lg:gap-[10px]">
                     <a>Waterproof</a>
                     <a>Windbreaker</a>
                     <a>Lightweight</a>
                 </div>
             </div>
-            <div class="flex flex-col">
-                <h1 class="font-bold lg:text-xl m-0 lg:mb-4">FOLLOW US</h1>
-                <div class="flex flex-row gap-[10px]">
-                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+            <div class="flex flex-col items-center">
+                <h1 class="font-bold lg:text-xl m-0 mb-4 lg:mb-4">FOLLOW US</h1>
+                <div class="flex flex-row lg:gap-[10px]">
+                    <div class="flex p-4">
                         <img src="@/assets/svg/facebook.svg" alt="">
                     </div>
-                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+                    <div class="flex p-4">
                         <img src="@/assets/svg/instagram.svg" alt="">
                     </div>
-                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+                    <div class="flex p-4">
                         <img src="@/assets/svg/youtube.svg" alt="">
                     </div>
-                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+                    <div class="flex p-4">
                         <img src="@/assets/svg/whatsapp.svg" alt="">
                     </div>
                 </div>
             </div>
         </div>
-        <div class="flex items-end justify-between text-white pt-[30px] relative border-[#5A7D80] border-t-[1px]">
-            <div class="flex flex-row items-center justify-center gap-[20px]">
-                <img src="@/assets/images/zezkun-logo-white.png" alt="">
-                <h1 class="text-white font-black text-[40px]">ZEZKUN</h1>
+        <div class="flex items-center flex-col sm:flex-row justify-between text-white pt-4 sm:py-[15px] lg:pt-[50px] relative border-[#5A7D80] border-t-[1px]">
+            <div class="hidden items-center justify-center gap-2 sm:gap-[10px] lg:gap-[20px] sm:flex sm:flex-row">
+                <img class="w-[20%] lg:w-full" src="@/assets/images/zezkun-logo-white.png" alt="">
+                <h1 class="text-white font-black text-[20px] lg:text-[40px]">ZEZKUN</h1>
             </div>
             <div class="flex items-center">
-                <p class="text-[16px] font-bold">© 2023 ZEZKUN- ALL RIGHTS RESERVED</p>
+                <p class="text-xs lg:text-[16px] font-bold">© 2023 ZEZKUN- ALL RIGHTS RESERVED</p>
             </div>
         </div>
     </div>
