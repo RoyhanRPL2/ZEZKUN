698078e7d4a8af50ff7ae006bb26b74bea32ce25
Jolangalifk <jolangalifk@gmail.com>
2024-05-15 11:01:27 +0700

    add: [JK] footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="m-0 lg:p-20 bg-[#0f1314] flex flex-col lg:gap-5 w-full">
+        <div class="flex justify-between text-white lg:py-9 relative">
+            <div class="flex flex-col">
+                <h1 class="font-bold lg:text-xl m-0 lg:mb-4">ZEZKUN</h1>
+                <div class="flex flex-col gap-[10px]">
+                    <a>Discover</a>
+                    <a>Our Story</a>
+                    <a>Join Us</a>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <h1 class="font-bold lg:text-xl m-0 lg:mb-4">INFORMATION</h1>
+                <div class="flex flex-col gap-[10px]">
+                    <a>My Account</a>
+                    <a>Contact Us</a>
+                    <a>FAQ</a>
+                    <a>Size Guide</a>
+                    <a>Shipping</a>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <h1 class="font-bold lg:text-xl m-0 lg:mb-4">CATEGORIES</h1>
+                <div class="flex flex-col gap-[10px]">
+                    <a>Waterproof</a>
+                    <a>Windbreaker</a>
+                    <a>Lightweight</a>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <h1 class="font-bold lg:text-xl m-0 lg:mb-4">FOLLOW US</h1>
+                <div class="flex flex-row gap-[10px]">
+                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+                        <img src="@/assets/svg/facebook.svg" alt="">
+                    </div>
+                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+                        <img src="@/assets/svg/instagram.svg" alt="">
+                    </div>
+                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+                        <img src="@/assets/svg/youtube.svg" alt="">
+                    </div>
+                    <div class="flex p-4 border-[#5A7D80] border-[1px]">
+                        <img src="@/assets/svg/whatsapp.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex items-end justify-between text-white pt-[30px] relative border-[#5A7D80] border-t-[1px]">
+            <div class="flex flex-row items-center justify-center gap-[20px]">
+                <img src="@/assets/images/zezkun-logo-white.png" alt="">
+                <h1 class="text-white font-black text-[40px]">ZEZKUN</h1>
+            </div>
+            <div class="flex items-center">
+                <p class="text-[16px] font-bold">© 2023 ZEZKUN- ALL RIGHTS RESERVED</p>
+            </div>
+        </div>
+    </div>
+</template>
