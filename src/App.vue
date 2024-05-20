ca33a113479e5d46c4f74ea3ddb2c24154035eea
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2024-05-20 19:22:10 +0700

    fix: [MW] fix conflict

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from '@/components/HeaderComponent.vue';
 </script>
 
 <template>
+
+  <HeaderComponent />
   <!-- <h1 class="text-7xl text-black">Hello World</h1> -->
 
   <RouterView />
