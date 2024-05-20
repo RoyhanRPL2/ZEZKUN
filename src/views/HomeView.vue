adf4d49fbb858f7098e163e9f76630b91668b336
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2024-05-20 15:59:02 +0700

    add: [MW] Category Section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
-import CarouselImage from '@/components/CarouselImage.vue'
+import CarouselImage from '@/components/CarouselImage.vue' 
+import CategoriesSection from '@/components/CategoriesSection.vue'
 </script>
 
 <template>
@@ -8,4 +9,5 @@ import CarouselImage from '@/components/CarouselImage.vue'
     <CarouselImage />
     <!-- <TheWelcome /> -->
   </main>
+  <CategoriesSection />
 </template>
