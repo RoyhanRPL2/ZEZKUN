d033099b601bfe81b5082c96f8b2cc2da2c6a1a5
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2024-05-20 22:44:58 +0700

    add: [MW] add Atc Section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@ import BrandsSection from '@/components/BrandsSection.vue';
 import TopProductSection from '@/components/TopProductSection.vue';
 import TestimoniSection from '@/components/TestimoniSection.vue';
 import BenefitSection from '@/components/BenefitSection.vue';
+import AtcSection from '@/components/AtcSection.vue';
 </script>
 
 <template>
@@ -18,4 +19,5 @@ import BenefitSection from '@/components/BenefitSection.vue';
   <TopProductSection />
   <TestimoniSection /> 
   <BenefitSection />
+  <AtcSection />
 </template>
